0d1cf88abedcab619240dcd15f3ebabc446285b8
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-06-04 21:34:10 -0500

    username search functioning

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const APIURL = "https://api.github.com/users/";
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
-getUser("wagnergabe");
+
 
 async function getUser(username) {
   try {
@@ -9,4 +11,16 @@ async function getUser(username) {
   } catch (err) {
     console.log(err);
   }
-}
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value;
+
+    if(user) {
+        getUser(user)
+
+        search.value = ""
+    }
+})
